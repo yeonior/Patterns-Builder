46a0a2920b02351ccb8b7ad8a4208e96079fc1ce
Rus <de.mr44@gmail.com>
2022-01-06 23:39:36 +0500

    Added CatalogPresenter class with protocol

---
+++ Patterns-Builder/Catalog/CatalogPresenter.swift
@@ -0,0 +1,18 @@
+//
+//  CatalogPresenter.swift
+//  Patterns-Builder
+//
+//  Created by Ruslan on 06.01.2022.
+//
+
+import Foundation
+
+protocol Presenter {
+    var info: String { get }
+}
+
+class CatalogPresenter: Presenter {
+    
+//    weak var viewController: CatalogViewController?
+    var info: String = "Creation a model for a view"
+}
